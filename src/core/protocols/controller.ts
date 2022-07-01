f52ccdee81b626e79f2387737106770a87cf0177
Zacarias, Lucas <zacariasoliveira56@gmail.com>
2022-07-01 17:43:01 -0300

    fix: configure pipeline

--- src/core/protocols/controller.ts
+++ src/core/protocols/controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common'
 export class AppController {
   @Get()
   initial(): string {
-    return 'Run application'
+    return 'Run application - initial route'
   }
 }
