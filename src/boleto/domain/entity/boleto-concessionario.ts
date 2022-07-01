f83e4a9ac3369fad8e92187c3774212e1e083d6a
Zacarias, Lucas <zacariasoliveira56@gmail.com>
2022-07-01 13:45:35 -0300

    fix: run lint fix

--- src/boleto/domain/entity/boleto-concessionario.ts
+++ src/boleto/domain/entity/boleto-concessionario.ts
@@ -1,20 +1,20 @@
-import { InvalidParamError } from '@/core/domain';
+import { InvalidParamError } from '@/core/domain'
 
 import { Boleto } from './boleto'
 
 export class BoletoConcessionario extends Boleto {
-  expirationDate: Date;
+  expirationDate: Date
 
   constructor(linhaDigitavel: string) {
     super(linhaDigitavel)
-    this.validator();
+    this.validator()
   }
 
   private validator() {
     if (this.linhaDigitavel.length !== 48) {
-      throw new InvalidParamError('linha digitada', 'A linha digitada deve possuir 48 números');
+      throw new InvalidParamError('linha digitada', 'A linha digitada deve possuir 48 números')
     }
-    this.validarDigitosVerificadores();
+    this.validarDigitosVerificadores()
   }
 
   public getBarCodeIncompleto() {
@@ -24,32 +24,32 @@ export class BoletoConcessionario extends Boleto {
       this.linhaDigitavel.slice(12, 23) +
       this.linhaDigitavel.slice(21, 31) +
       this.linhaDigitavel.slice(32, 47)
-    );
+    )
   }
 
   public obterCodigoDeBarra() {
-    const incompleto = this.getBarCodeIncompleto();
-    const digitoVerificador = this.calculoModulo11();
+    const incompleto = this.getBarCodeIncompleto()
+    const digitoVerificador = this.calculoModulo11()
 
-    return `${incompleto.slice(0, 4)}${digitoVerificador}${incompleto.slice(4)}`;
+    return `${incompleto.slice(0, 4)}${digitoVerificador}${incompleto.slice(4)}`
   }
 
   private validarDigitosVerificadores() {
-    const primeiroDigito = super.calculoModulo10(this.linhaDigitavel.slice(0, 11), 2);
-    const segundoDigito = super.calculoModulo10(this.linhaDigitavel.slice(12, 23), 2);
-    const terceiroDigito = super.calculoModulo10(this.linhaDigitavel.slice(24, 35), 2);
-    const quartoDigito = super.calculoModulo10(this.linhaDigitavel.slice(36, 47), 2);
+    const primeiroDigito = super.calculoModulo10(this.linhaDigitavel.slice(0, 11), 2)
+    const segundoDigito = super.calculoModulo10(this.linhaDigitavel.slice(12, 23), 2)
+    const terceiroDigito = super.calculoModulo10(this.linhaDigitavel.slice(24, 35), 2)
+    const quartoDigito = super.calculoModulo10(this.linhaDigitavel.slice(36, 47), 2)
 
     if (primeiroDigito !== Number(this.linhaDigitavel[11]))
-      throw new InvalidParamError('boleto', 'erro no digito verificador 1.');
+      throw new InvalidParamError('boleto', 'erro no digito verificador 1.')
 
     if (segundoDigito !== Number(this.linhaDigitavel[23]))
-      throw new InvalidParamError('boleto', 'erro no digito verificador 2.');
+      throw new InvalidParamError('boleto', 'erro no digito verificador 2.')
 
     if (terceiroDigito !== Number(this.linhaDigitavel[35]))
-      throw new InvalidParamError('boleto', 'erro no digito verificador 3.');
+      throw new InvalidParamError('boleto', 'erro no digito verificador 3.')
 
     if (quartoDigito !== Number(this.linhaDigitavel[47]))
-      throw new InvalidParamError('boleto', 'erro no digito verificador 4.');
+      throw new InvalidParamError('boleto', 'erro no digito verificador 4.')
   }
 }
--- src/boleto/protocols/boleto.controller.ts
+++ src/boleto/protocols/boleto.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { BuscarBoleto } from '@/boleto/data';
-import { BoletoBancario, BoletoConcessionario } from '../domain';
+import { Controller, Get, Param } from '@nestjs/common'
+import { BuscarBoleto } from '@/boleto/data'
+import { BoletoBancario, BoletoConcessionario } from '../domain'
 
 @Controller('boleto')
 export class BoletoController {
   @Get('/:linhaDigitada')
   async teste(@Param('linhaDigitada') linha: string): Promise<BoletoBancario | BoletoConcessionario> {
-    const useCase = new BuscarBoleto();
+    const useCase = new BuscarBoleto()
 
-    return await useCase.execute(linha);
+    return await useCase.execute(linha)
   }
 }
